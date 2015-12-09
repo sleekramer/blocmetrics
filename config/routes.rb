35d5dc5a33c4a8ab8ada636cc702286faacf94ac
Steven Lee-Kramer <sleekramer@gmail.com>
2015-12-09 12:58:03 -0500

    Add registered_application model and controller with CRUD actions filled out and completed views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [] do
+    resources :registered_applications
+  end
+
+  authenticated :user do
+    root 'registered_applications#index', as: :authenticated
+  end
+
   devise_for :users
   root to: 'welcome#about'
 
